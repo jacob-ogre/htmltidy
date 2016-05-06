7b71c202b97a8e7e4bba20b9c7bede43710b46b7
boB Rudis <bob@rudis.net>
2016-05-06 18:05:43 -0400

    changed test to work with multiple libraries

--- tests/testthat/test-htmltidy.R
+++ tests/testthat/test-htmltidy.R
@@ -1,7 +1,7 @@
 context("basic functionality")
 test_that("we can do something", {
 
-  expect_that(nchar(tidy("<b><p><a href='http://google.com'>google &gt</a></p></b>")),
-              equals(256))
+  expect_gte(nchar(tidy("<b><p><a href='http://google.com'>google &gt</a></p></b>")),
+             256)
 
 })
