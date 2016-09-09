63eb2bb5b0ca00840324ce73371fb368cafc13cf
hrbrmstr <bob@rudis.net>
2016-09-09 15:36:18 -0400

    README

--- R/htmltidy-package.r
+++ R/htmltidy-package.r
@@ -1,8 +1,12 @@
-#' Clean up gnarly HTML/XML
+#' Clean Up Gnarly HTML/XML
+#'
+#' HTML and XML documents can be beautiful and pristine. They can also be
+#' wretched, evil, malformed hellspawn. Now, you can tidy up that HTML and XML before
+#' processing it with your favorite angle-bracket parsing tools.
 #'
 #' @name htmltidy
 #' @docType package
-#' @author Bob Rudis (@@hrbrmstr)
+#' @author Bob Rudis (bob@@rud.is)
 #' @useDynLib htmltidy
 #' @importFrom Rcpp sourceCpp
 NULL
